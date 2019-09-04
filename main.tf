e08f02b89bec32eb1d9b708838930b104cde2f30
Rohit Verma <38323616+rverma-nikiai@users.noreply.github.com>
2019-09-04 13:27:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-var "enable_logging" {
-  default = false
-}
-
 module "origin_label" {
   source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.4.0"
   namespace  = var.namespace
